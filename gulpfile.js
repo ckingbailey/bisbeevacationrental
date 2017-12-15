70b48eace12ef8da031f45a832715446104ba3e7
Colin <ckingbailey@gmail.com>
2017-12-15 11:10:19 -0800

    Mobile optimize (#4)
    * create dist folder and move dist files in there. crop some images.
    
    * install gulp-imagemin
    
    * reduce jpg quality of large images
    
    * compress images with PS Save For Web & Devics and gulp-imagemin
    
    * start a little test page within the site to figure out some peculiarites of Foundation and find an alternative for position: fixed.
    
    * install gulp-sass in preparation for fixing the position:fixed issue with the background
    
    * add meta viewport tag, change the way bg img sizes to get it working on mobile.
    
    * change the way the background sizes, attaches AGAIN.
    
    * break arrows in trying to attach them to the bottom of the photo slider
    
    * slide carousel arrows and dots work and look good. Let us never nest multiple unrelated branches again.
    
    * afix arrows to outer nav container and dots to inner nav container
    
    * tighten up slider nav to fit in mobile display
    
    * navbar lighten font weight and increase letter-spacing. hopefully it fits on mobile.
    
    * rewrote and cleaned up css
    
    * min-height 100 for maincontainerwrap
    
    * gitignore some stuff
    
    * gitignore some more stuff
    
    * delete old css files
    
    * delete sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var imagemin = require('imagemin');
-var del = require('del');
-var rename = require('gulp-rename');
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+
+function minifyImgs() {
+  return gulp.src('photos/keepers/*.jpg')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/photos'));
+}
+
+function compileSass() {
+  return gulp.src('scss/sassy-styles.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(rename('style-min.css')
+    .on('error', rename.logError))
+    .pipe(gulp.dest('dist/css'));
+}
+
+gulp.task('minifyImgs', minifyImgs);
+gulp.task('scss', compileSass);
